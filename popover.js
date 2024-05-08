5369e7619dc230a01445336e8e684849513ba9a1
Andres Rojas <andre.rojasm@duocuc.cl>
2024-05-08 16:14:52 -0400

    agregando nuevo proyecto

--- popover.js
+++
@@ -1,3 +0,0 @@
-// Coloca aquí el código para inicializar los popovers
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
