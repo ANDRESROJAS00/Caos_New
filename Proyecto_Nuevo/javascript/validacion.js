51813f66e5d58589febb39a1186aca4989d3e85f
Andres Rojas <andre.rojasm@duocuc.cl>
2024-05-13 03:07:50 -0400

    agregando detalles al formulario

--- Proyecto_Nuevo/javascript/validacion.js
+++ Proyecto_Nuevo/javascript/validacion.js
@@ -1,146 +1,116 @@
-(() => {
-    'use strict';
+$(document).ready(function () {
+    $('.needs-validation').submit(function (event) {
+        var form = $(this);
+        var isValid = true;
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
+        // Validación de email
+        var inputEmail = $('#emailInput', form);
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(inputEmail.val().trim())) {
+            isValid = false;
+            inputEmail.addClass('is-invalid');
+            inputEmail.next().html('Ingrese un correo electrónico válido.');
+        } else {
+            inputEmail.removeClass('is-invalid');
+            inputEmail.next().html('');
+        }
 
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            // Select the input element for address
-            const inputEmail = form.querySelector('#emailInput')
-            const passwordInput = form.querySelector('#inputPass')
-            const addressInput = form.querySelector('#inputAdress');
-            const addressInput2 = form.querySelector('#inputAdress2');
-            const inputCity = form.querySelector('#cityInput');
-            const inputPostalCode = form.querySelector('#postalCodeInput');
-            const inputCountry = form.querySelector('#countryInput');
-            const inputCheckTerms = form.querySelector('#checkTerms');
+        // Validación de contraseña
+        var passwordInput = $('#inputPass', form);
+        if (passwordInput.val().trim().length < 6) {
+            isValid = false;
+            passwordInput.addClass('is-invalid');
+            passwordInput.next().html('La contraseña debe tener al menos 6 caracteres.');
+        } else {
+            passwordInput.removeClass('is-invalid');
+            passwordInput.next().html('');
+        }
 
+        // Resto de las validaciones...
 
-            /*---==VALIDACION EMAIL==---*/
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(inputEmail.value.trim())) {
+        // Validación de dirección
+        var addressInput = $('#inputAdress', form);
+        if (addressInput.val().trim().length < 5) {
+            isValid = false;
+            addressInput.addClass('is-invalid');
+            addressInput.next().html('La dirección debe tener al menos 5 caracteres.');
+        } else {
+            addressInput.removeClass('is-invalid');
+            addressInput.next().html('');
+        }
 
-                event.preventDefault();
-                event.stopPropagation();
+        // Validación de dirección 2
+        var addressInput2 = $('#inputAdress2', form);
+        if (addressInput2.val().trim().length < 5) {
+            isValid = false;
+            addressInput2.addClass('is-invalid');
+            addressInput2.next().html('La dirección debe tener al menos 5 caracteres.');
+        } else {
+            addressInput2.removeClass('is-invalid');
+            addressInput2.next().html('');
+        }
 
-                inputEmail.classList.add('is-invalid');
-                inputEmail.nextElementSibling.innerHTML = 'Ingrese un correo electrónico válido.';
-            } else {
-                inputEmail.classList.add('is-invalid');
-                inputEmail.nextElementSibling.innerHTML = '';
-            }
+        // Validación de ciudad
+        var inputCity = $('#cityInput', form);
+        if (inputCity.val().trim().length < 4) {
+            isValid = false;
+            inputCity.addClass('is-invalid');
+            inputCity.next().html('La ciudad debe tener al menos 4 caracteres.');
+        } else {
+            inputCity.removeClass('is-invalid');
+            inputCity.next().html('');
+        }
 
+        // Validación de código postal
+        var inputPostalCode = $('#postalCodeInput', form);
+        var postalCodeRegex = /^\d+$/;
+        if (!postalCodeRegex.test(inputPostalCode.val().trim())) {
+            isValid = false;
+            inputPostalCode.addClass('is-invalid');
+            inputPostalCode.next().html('El código postal debe contener solo números.');
+        } else if (inputPostalCode.val().trim().length < 4) {
+            isValid = false;
+            inputPostalCode.addClass('is-invalid');
+            inputPostalCode.next().html('El código postal debe tener al menos 4 caracteres.');
+        } else {
+            inputPostalCode.removeClass('is-invalid');
+            inputPostalCode.next().html('');
+        }
 
+        // Validación de país
+        var inputCountry = $('#countryInput', form);
+        if (inputCountry.val().trim().length < 4) {
+            isValid = false;
+            inputCountry.addClass('is-invalid');
+            inputCountry.next().html('El país debe tener al menos 4 caracteres.');
+        } else {
+            inputCountry.removeClass('is-invalid');
+            inputCountry.next().html('');
+        }
 
-            /*---==VALIDACION PASSWORD==---*/
-            if (passwordInput.value.trim().length < 6) {
+        // Validación de aceptación de términos
+        var inputCheckTerms = $('#checkTerms', form);
+        if (!inputCheckTerms.prop('checked')) {
+            isValid = false;
+            inputCheckTerms.addClass('is-invalid');
+            inputCheckTerms.next().html('Debes aceptar los términos para continuar.');
+        } else {
+            inputCheckTerms.removeClass('is-invalid');
+            inputCheckTerms.next().html('');
+        }
 
-                event.preventDefault();
-                event.stopPropagation();
-
-                passwordInput.classList.add('is-invalid');
-                passwordInput.nextElementSibling.innerHTML = 'La contraseña debe tener al menos 6 caracteres.';
-            } else {
-                passwordInput.classList.remove('is-invalid');
-                passwordInput.nextElementSibling.innerHTML = '';
-            }
-
-
-            /*---==VALIDACION DIRECCIÓN==---*/
-            if (addressInput.value.trim().length < 5) {
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                addressInput.classList.add('is-invalid');
-                addressInput.nextElementSibling.innerHTML = 'La dirección debe tener al menos 5 caracteres.';
-            } else {
-
-                addressInput.classList.remove('is-invalid');
-                addressInput.nextElementSibling.innerHTML = '';
-            }
-
-            /*---==VALIDACION DIRECCIÓN 2==---*/
-            if (addressInput2.value.trim().length < 5) {
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                addressInput2.classList.add('is-invalid');
-                addressInput2.nextElementSibling.innerHTML = 'La dirección debe tener al menos 5 caracteres.';
-            } else {
-
-                addressInput2.classList.remove('is-invalid');
-                addressInput2.nextElementSibling.innerHTML = '';
-            }
-
-            /*---==VALIDACION CIUDAD==---*/
-            if (inputCity.value.trim().length < 4) {
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                inputCity.classList.add('is-invalid');
-                inputCity.nextElementSibling.innerHTML = 'La ciudad debe tener al menos 4 caracteres.';
-            } else {
-                inputCity.classList.remove('is-invalid');
-                inputCity.nextElementSibling.innerHTML = '';
-            }
-
-            /*---==VALIDACION CÓDIGO POSTAL==---*/
-            const postalCodeRegex = /^\d+$/; // Expresión regular que permite solo números
-            if (!postalCodeRegex.test(inputPostalCode.value.trim())) { // Verifica si el valor contiene solo números
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                inputPostalCode.classList.add('is-invalid'); // Agrega la clase 'is-invalid' para mostrar que hay un error
-                inputPostalCode.nextElementSibling.innerHTML = 'El código postal debe contener solo números.'; // Muestra un mensaje de error
-            } else if (inputPostalCode.value.trim().length < 4) { // Verifica si el código postal tiene menos de 4 caracteres
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                inputPostalCode.classList.add('is-invalid'); // Agrega la clase 'is-invalid' para mostrar que hay un error
-                inputPostalCode.nextElementSibling.innerHTML = 'El código postal debe tener al menos 4 caracteres.'; // Muestra un mensaje de error
-            } else {
-                inputPostalCode.classList.remove('is-invalid'); // Si la validación es correcta, quita la clase 'is-invalid'
-                inputPostalCode.nextElementSibling.innerHTML = ''; // Borra el mensaje de error
-            }
-
-
-            /*---==VALIDACION PAIS==---*/
-            if (inputCountry.value.trim().length < 4) {
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                inputCountry.classList.add('is-invalid');
-                inputCountry.nextElementSibling.innerHTML = 'La ciudad debe tener al menos 4 caracteres.';
-            } else {
-                inputCountry.classList.remove('is-invalid');
-                inputCountry.nextElementSibling.innerHTML = '';
-            }
+       
+        if (!isValid) {
+            event.preventDefault();
+            event.stopPropagation();
+            alert('Faltan campos por rellenar.');
+        } else {
+            alert('Registrado correctamente');
+            console.log('Registrado correctamente');
+        }
+        
 
 
-
-            /*---==VALIDACION ACEPTACIÓN DE TÉRMINOS==---*/
-            if (!inputCheckTerms.checked) { // Verifica si el checkbox no está marcado
-
-                event.preventDefault();
-                event.stopPropagation();
-
-                inputCheckTerms.classList.add('is-invalid'); // Agrega la clase 'is-invalid' para mostrar que hay un error
-                inputCheckTerms.nextElementSibling.innerHTML = 'Debes aceptar los términos para continuar.'; // Muestra un mensaje de error
-            } else {
-                inputCheckTerms.classList.remove('is-invalid'); // Si se ha marcado el checkbox, quita la clase 'is-invalid'
-                inputCheckTerms.nextElementSibling.innerHTML = ''; // Borra el mensaje de error
-            }
-
-
-            form.classList.add('was-validated');
-        }, false);
+        form.addClass('was-validated');
     });
-})();
+});
