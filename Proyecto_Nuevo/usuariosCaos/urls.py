14423101dec452e4a46e00dca91d773a082af4d5
andres <andres.rojascontacto@gmail.com>
2024-06-17 19:47:09 -0400

    subiendo actualizacion

--- Proyecto_Nuevo/usuariosCaos/urls.py
+++ Proyecto_Nuevo/usuariosCaos/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
 
-    path('crud', views , crud, name='crud'),
+    path('crud', views.crud, name='crud'),
     path('usuariosAdd',views.usuariosAdd, name='usuariosAdd'),
 ]
